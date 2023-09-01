fe5ed83f39ad4fbff493a4c22787ab56c1b3eab9
azaria-morake <azariamorake@gmail.com>
2023-09-01 02:52:11 -0700

    This is mandatory batch

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: represents the number of arguments
+ * @argv: is an array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+  int i;
+
+  for (i = 0; i < argc; i++)
+    {
+      printf("%s\n", argv[i]);
+    }
+
+  return (0);
+}
