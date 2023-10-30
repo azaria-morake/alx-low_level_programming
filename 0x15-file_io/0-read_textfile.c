910d80e65ac54e6f4055eef3f5c3880c83f37730
azaria-morake <141196419+azaria-morake@users.noreply.github.com>
2023-10-30 21:38:32 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* read_textfile- Read text file print to STDOUT.
-* @filename: text file being read
-* @letters: number of letters to be read
-* Return: w- actual number of bytes read and printed
-* 0 when function fails or filename is NULL.
-*/
+ * read_textfile- Reads a text file and prints
+ * its contents to Standard Output (STDOUT).
+ * @filename: The name of the text file to be read.
+ * @letters: The number of letters to be read and printed.
+ * Return: The actual number of bytes read and printed.
+ *        0 when the function fails or when 'filename' is NULL.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
-free(buf);
-close(fd);
-return (w);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
