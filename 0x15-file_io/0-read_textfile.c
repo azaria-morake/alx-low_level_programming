ad4ebc0e77a4b2c23fbca3bc52a79e2a3b287512
azaria-morake <141196419+azaria-morake@users.noreply.github.com>
2023-10-30 18:45:39 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,17 +9,42 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
-free(buf);
-close(fd);
-return (w);
+	char *buffer;
+	int count, fd_o, fd_r;
+
+	if (filename == NULL)
+		return (0);
+
+	/* Open file and get file descriptor */
+	fd_o = open(filename, O_RDONLY);
+	if (fd_o == -1)
+		return (0);
+
+	/* allocate memory in buffer to read contents of fd */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	/* read contents of fd */
+	fd_r = read(fd_o, buffer, letters);
+	if (fd_r == 0)
+	{
+		free(buffer);
+		close(fd_o);
+		return (0);
+	}
+
+	/* write to standard output */
+	count = write(STDOUT_FILENO, buffer, fd_r);
+	if (count == -1)
+	{
+		free(buffer);
+		close(fd_r);
+		close(fd_o);
+		return (0);
+	}
+	close(fd_r);
+	close(fd_o);
+
+	return (count);
 }
