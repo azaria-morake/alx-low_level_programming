0183a0331f2aeec0c1d3169aed7f8966aa9ef6fa
azaria-morake <141196419+azaria-morake@users.noreply.github.com>
2023-10-30 18:01:53 +0200

    Add files via upload

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Reads a text file and prints
+ * its contents to Standard Output (STDOUT).
+ * @filename: The name of the text file to be read.
+ * @letters: The number of letters to be read and printed.
+ * Return: The actual number of bytes read and printed.
+ *        0 when the function fails or when 'filename' is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
